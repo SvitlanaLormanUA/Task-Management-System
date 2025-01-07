2c9df9814fad7b2e7bc8c698376e36c4dcd888a3
Svitlana Lorman <svlormanua@gmail.com>
2025-01-07 21:53:19 +0200

    updated db models with enums

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,23 @@
+from enum import Enum
 from config import db
 
-# багато-до-багатьох
+# Багато-до-багатьох
 user_task = db.Table('user_task',
     db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
     db.Column('task_id', db.Integer, db.ForeignKey('tasks.id'), primary_key=True)
 )
+
+class TaskCategory(Enum):
+    WORK = "Work"
+    HOME = "Home"
+    STUDY = "Study"
+    OTHER = "Other"
+class TaskStatus(Enum):
+    PENDING = "Pending"
+    IN_PROGRESS = "In Progress"
+    COMPLETED = "Completed"
+    CANCELED = "Canceled"
+    
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -26,32 +39,6 @@ class User(db.Model):
             "tasks": [task.id for task in self.tasks]
         }
 
-class Category(db.Model):
-    __tablename__ = 'categories'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    tasks = db.relationship('Task', back_populates='category')  # Взаємозв’язок через back_populates
-
-    def to_json(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "tasks": [task.id for task in self.tasks]
-        }
-
-class Status(db.Model):
-    __tablename__ = 'statuses'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    tasks = db.relationship('Task', back_populates='status')  # Взаємозв’язок через back_populates
-
-    def to_json(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "tasks": [task.id for task in self.tasks]
-        }
-
 class Task(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
@@ -59,16 +46,11 @@ class Task(db.Model):
     description = db.Column(db.Text, nullable=True)
     date_assigned = db.Column(db.DateTime, nullable=True)
     date_due = db.Column(db.DateTime, nullable=True)
-    
+
     users = db.relationship('User', secondary=user_task, back_populates='tasks')
 
-    # Foreign key and relationship with Status
-    status_id = db.Column(db.Integer, db.ForeignKey('statuses.id'), nullable=True)
-    status = db.relationship('Status', back_populates='tasks')
-                             
-    # Foreign key and relationship with Category
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=True)
-    category = db.relationship('Category', back_populates='tasks')
+    status = db.Column(db.Enum(TaskStatus), nullable=False, default=TaskStatus.PENDING)
+    category = db.Column(db.Enum(TaskCategory), nullable=True)
 
     def to_json(self):
         return {
@@ -78,6 +60,6 @@ class Task(db.Model):
             "dateAssigned": self.date_assigned,
             "dateDue": self.date_due,
             "users": [user.id for user in self.users],
-            "status": self.status_id,
-            "category": self.category_id
+            "status": self.status.value,  
+            "category": self.category.value if self.category else None
         }
