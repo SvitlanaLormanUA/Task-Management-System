d9042ae1b288519167fd8f45699b7c3a67fe4871
alina <alina230605@gmail.com>
2025-06-04 23:18:17 +0300

    /feat/ view of all settings pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,29 @@
 import MainPage from './pages/MainPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import ProfilePage from './pages/ProfilePage';
 import SettingsPage from './pages/SettingsPage';
+import NotificationSettings from "@/pages/NotificationSettings.tsx";
+import PrivacySettings from "@/pages/PrivacySettings.tsx";
+import LanguageAndRegion from "@/pages/LanguageAndRegion.tsx";
+
 function App() {
 
-  return (
-    <div className='bg-blue-100'>
-    <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<MainPage />}>
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-       </Route>
-    </Routes>
-   </BrowserRouter>
-   </div>
-  );
+    return (
+        <div className='bg-blue-100'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LanguageAndRegion/>}>
+                        <Route path="/profile" element={<ProfilePage/>}/>
+                        <Route path="/settings" element={<SettingsPage/>}/>
+                        <Route path="/settings/notification-settings" element={<NotificationSettings/>}/>
+                        <Route path="/settings/privacy-settings" element={<PrivacySettings/>}/>
+                        <Route path="/settings/language-settings" element={<LanguageAndRegion/>}/>
+
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
+
 export default App;
