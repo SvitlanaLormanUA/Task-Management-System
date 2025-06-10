dcf741f94ef7e735fca04a811485230c9a539027
alina <alina230605@gmail.com>
2025-06-11 02:15:52 +0300

    /feat/ view of matrix page

--- frontend/src/pages/ToDoListPage.tsx
+++ frontend/src/pages/ToDoListPage.tsx
@@ -66,9 +66,9 @@ const ToDoListPage = () => {
           {[
             { title: 'calendar', color: '#FAFAF5' },
             { title: 'matrix', color: '#FBD443' },
-            { title: 'quick notes', color: '#FEF9F5' },
+          //  { title: 'quick notes', color: '#FEF9F5' },
             { title: 'habit-tracker', color: '#FFF7D8' },
-            { title: 'goals', color: '#F3D9DA' },
+            { title: 'goals | beta', color: '#F3D9DA' },
           ].map((tab) => (
             <Square
               key={tab.title}
